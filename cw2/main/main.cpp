e99d7fb19ad73539224633940cc6b4526ae9c7e5
frankRenlf <89248496+frankRenlf@users.noreply.github.com>
2023-01-03 18:42:01 +0800

    add

--- cw2/main/main.cpp
+++ cw2/main/main.cpp
@@ -1,6 +1,10 @@
 #include <glad.h>
 #include <GLFW/glfw3.h>
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 #include <typeinfo>
 #include <stdexcept>
 
